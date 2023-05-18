33b727b82d5f23a809c0f1529cd6f7e65b920c44
Guz <DarckProyect8@gmail.com>
2023-05-18 01:06:11 -0500

    Cambios en el formulario de producto y solucion de errores de imagenes

--- app/src/main/java/com/paqta/paqtafood/screens/product/components/AddProductFragment.java
+++ app/src/main/java/com/paqta/paqtafood/screens/product/components/AddProductFragment.java
@@ -7,6 +7,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -15,6 +17,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -31,10 +34,12 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -62,11 +67,6 @@ public class AddProductFragment extends Fragment {
     private FirebaseStorage mStorage;
     private FirebaseAuth mAuth;
     StorageReference storageReference;
-
-//    private static final int COD_SEL_STORAGE = 200;
-//    private static final int COD_SEL_IMAGE = 300;
-//    private static final int MY_CAMERA_REQUEST_CODE = 100;
-//    private static final int PICK_IMAGE = 1;
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_IMAGE_GALLERY = 2;
@@ -114,6 +114,7 @@ public class AddProductFragment extends Fragment {
                         .setPositiveButton("Galeria", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                permisosCamara();
                                 openImageGallery();
                             }
                         })
@@ -153,9 +154,7 @@ public class AddProductFragment extends Fragment {
                     String descripcion = edtTxtDescripcion.getText().toString().trim();
                     String categoria = edtTxtCategoria.getText().toString().trim();
 
-                    if (nombre.isEmpty() && descripcion.isEmpty() && categoria.isEmpty()) {
-                        Toast.makeText(getContext(), "Ingresar los datos", Toast.LENGTH_SHORT).show();
-                    } else {
+                    if (validar(nombre, descripcion, categoria)) {
                         postProduct(nombre, descripcion, categoria);
                     }
                 }
@@ -170,17 +169,46 @@ public class AddProductFragment extends Fragment {
                     String descripcion = edtTxtDescripcion.getText().toString().trim();
                     String categoria = edtTxtCategoria.getText().toString().trim();
 
-                    if (nombre.isEmpty() && descripcion.isEmpty() && categoria.isEmpty()) {
-                        Toast.makeText(getContext(), "Ingresar los datos", Toast.LENGTH_SHORT).show();
-                    } else {
+                    if (validar(nombre, descripcion, categoria)) {
                         updateProduct(nombre, descripcion, categoria);
                     }
                 }
             });
         }
 
-        permisosCamara();
         return root;
+    }
+
+    /**
+     * Valida los campos y si la imagen es diferente que la de por defecto o en todo caso si es diferente a la anterior imagen
+     *
+     * @param nombre
+     * @param descripcion
+     * @param categoria
+     * @return
+     */
+    private boolean validar(String nombre, String descripcion, String categoria) {
+        boolean isValidate = false;
+
+        if (nombre.isEmpty() && descripcion.isEmpty() && categoria.isEmpty()) {
+            Toast.makeText(getContext(), "Ingresar los datos", Toast.LENGTH_SHORT).show();
+        } else if (photo_prod.getDrawable() == null) {
+            Toast.makeText(getContext(), "Selecciona una imagen", Toast.LENGTH_SHORT).show();
+        } else {
+            Drawable currentDrawable = photo_prod.getDrawable();
+            Drawable defaultDrawable = getResources().getDrawable(R.drawable.image_icon_124);
+
+            Bitmap currentBitmap = ((BitmapDrawable) currentDrawable).getBitmap();
+            Bitmap defaultBitmap = ((BitmapDrawable) defaultDrawable).getBitmap();
+
+            if (currentBitmap.equals(defaultBitmap)) {
+                Toast.makeText(getContext(), "Selecciona una imagen diferente", Toast.LENGTH_SHORT).show();
+            } else {
+                isValidate = true;
+            }
+        }
+
+        return isValidate;
     }
 
     /**
@@ -209,9 +237,7 @@ public class AddProductFragment extends Fragment {
      */
     private void openCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-//        }
     }
 
     @Override
@@ -220,71 +246,15 @@ public class AddProductFragment extends Fragment {
         if (resultCode == RESULT_OK) {
             if (requestCode == REQUEST_IMAGE_GALLERY) {
                 image_url = data.getData();
-                Glide.with(this).load(image_url).into(photo_prod);
-
+                Glide.with(getView()).load(image_url).into(photo_prod);
             }
             if (requestCode == REQUEST_IMAGE_CAPTURE) {
                 image_camera = (Bitmap) data.getExtras().get("data");
                 image_url = bitmapToUri(image_camera);
-                Glide.with(this).load(image_url).into(photo_prod);
-                Log.d("imagen", image_url.toString());
-
+                Glide.with(getView()).load(image_url).into(photo_prod);
             }
         }
     }
-
-
-    private void subirImagen(Uri image_url) {
-        //        ProgressDialog progressDialog = new ProgressDialog(getContext());
-//        progressDialog.setMessage("Actualizando imagen");
-//        progressDialog.show();
-
-        String ruta_storage_foto = storage_path + "" + photo + "" + mAuth.getUid() + "" + id_prod;
-        StorageReference reference = mStorage.getReference().child(ruta_storage_foto);
-        reference.putFile(image_url).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-
-            }
-        });
-
-    }
-
-
-
-//    private void subirFoto(Uri image_url) {
-//        ProgressDialog progressDialog = new ProgressDialog(getContext());
-//        progressDialog.setMessage("Actualizando imagen");
-//        progressDialog.show();
-//        String ruta_storage_foto = storage_path + "" + photo + "" + mAuth.getUid() + "" + id_prod;
-//        StorageReference reference = mStorage.getReference().child(ruta_storage_foto);
-//        reference.putFile(image_url).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//            @Override
-//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-//                while (!uriTask.isSuccessful()) {
-//                    if (uriTask.isSuccessful()) {
-//                        uriTask.addOnSuccessListener(new OnSuccessListener<Uri>() {
-//                            @Override
-//                            public void onSuccess(Uri uri) {
-//                                String download_uri = uri.toString();
-//                                HashMap<String, Object> map = new HashMap<>();
-//                                map.put("imagen", download_uri);
-//                                mFirestore.collection("productos").document(id_prod).update(map);
-//                                Toast.makeText(getContext(), "Foto actualizada", Toast.LENGTH_SHORT).show();
-//                                progressDialog.dismiss();
-//                            }
-//                        });
-//                    }
-//                }
-//            }
-//        });
-//    }
 
     /**
      * Encargado de modificar el producto utilizando FireStorage
@@ -294,22 +264,29 @@ public class AddProductFragment extends Fragment {
      * @param categoria
      */
     private void updateProduct(String nombre, String descripcion, String categoria) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("nombre", nombre);
-        map.put("descripcion", descripcion);
-        map.put("categoria", categoria);
 
-        mFirestore.collection("productos").document(id_prod).update(map)
+        DocumentReference documentReference = mFirestore.collection("productos").document(id_prod);
+
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("nombre", nombre);
+        updates.put("descripcion", descripcion);
+        updates.put("categoria", categoria);
+
+        documentReference.update(updates)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
-                        Toast.makeText(getContext(), "Actualizado exitosamente", Toast.LENGTH_SHORT).show();
+                        if (image_url != null) {
+                            subirImagen(documentReference);
+                        }
+                        Toast.makeText(getContext(), "El producto se modificó exitosamente", Toast.LENGTH_SHORT).show();
                         replaceFragment(new ProductFragment());
                     }
-                }).addOnFailureListener(new OnFailureListener() {
+                })
+                .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getContext(), "Error al actualizar", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Error al modificar el producto", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
@@ -322,24 +299,63 @@ public class AddProductFragment extends Fragment {
      * @param categoria
      */
     private void postProduct(String nombre, String descripcion, String categoria) {
+
+        DocumentReference documentReference = mFirestore.collection("productos").document();
+
         Map<String, Object> map = new HashMap<>();
         map.put("nombre", nombre);
         map.put("descripcion", descripcion);
         map.put("categoria", categoria);
 
-        mFirestore.collection("productos").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        documentReference.set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        subirImagen(documentReference);
+                        replaceFragment(new ProductFragment());
+                    }
+                });
+    }
+
+    /**
+     * Se encarga de subir la imagen a FirebaseStorage con la referencia del documento que se envia por parametro
+     *
+     * @param documentReference
+     */
+    private void subirImagen(DocumentReference documentReference) {
+        String ruta_storage_foto = storage_path + "" + photo + "" + mAuth.getUid() + "" + documentReference.getId();
+        StorageReference imageRef = mStorage.getReference().child(ruta_storage_foto);
+
+        UploadTask uploadTask = imageRef.putFile(image_url);
+        uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
             @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Toast.makeText(getContext(), "Creado exitosamente", Toast.LENGTH_SHORT).show();
-                replaceFragment(new ProductFragment());
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getContext(), "Error al registrar", Toast.LENGTH_SHORT).show();
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+                    imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String imageUrl = uri.toString();
+
+                            documentReference.update("imagen", imageUrl)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Toast.makeText(getContext(), "El documento se subió con su imagen exitosamente", Toast.LENGTH_SHORT).show();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(getContext(), "Error al subir el documento con su imagen", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        }
+                    });
+                }
             }
         });
     }
+
 
     /**
      * Obtiene el producto segun el id que fue enviado desde el boton de editar del anterior fragment
@@ -360,14 +376,17 @@ public class AddProductFragment extends Fragment {
                         edtTxtDescripcion.setText(descripcionProducto);
                         edtTxtCategoria.setText(categoriaProducto);
 
-                        Toast.makeText(getContext(), "Cargando foto", Toast.LENGTH_SHORT).show();
-                        Glide.with(getContext()).load(imagenProducto).into(photo_prod);
-
+                        if (imagenProducto == null) {
+                            photo_prod.setImageResource(R.drawable.image_icon_124);
+                        } else {
+                            Glide.with(getView()).load(imagenProducto).into(photo_prod);
+                            Snackbar.make(getView(), "Cargando Foto", Snackbar.LENGTH_LONG).show();
+                        }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getContext(), "Error al obtener los datos", Toast.LENGTH_SHORT).show();
+                        Snackbar.make(getView(), "Error al obtener los datos", Snackbar.LENGTH_LONG).show();
                     }
                 });
     }
@@ -387,6 +406,7 @@ public class AddProductFragment extends Fragment {
 
     /**
      * Convierte un Bitmap a una URI
+     *
      * @param bitmap
      * @return
      */
