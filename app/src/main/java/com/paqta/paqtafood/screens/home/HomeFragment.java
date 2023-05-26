e7384a37bf6653ac1624a68b5938ed57c373ed62
Guz <DarckProyect8@gmail.com>
2023-05-26 15:56:08 -0500

    Generacion de qr dinamico

--- app/src/main/java/com/paqta/paqtafood/screens/home/HomeFragment.java
+++ app/src/main/java/com/paqta/paqtafood/screens/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.paqta.paqtafood.screens.home;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
 import android.os.FileUtils;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,13 +23,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
@@ -43,6 +53,7 @@ import com.paqta.paqtafood.model.Producto;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -51,8 +62,9 @@ import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
-    String filePath;
-    FirebaseFirestore mFirestore;
+    String storagePathPdfCartilla = "archivos/cartilla.pdf";
+    private FirebaseFirestore mFirestore;
+    private FirebaseStorage mStorage;
     ImageView qr;
     Button button;
 
@@ -65,22 +77,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
         mFirestore = FirebaseFirestore.getInstance();
+        mStorage = FirebaseStorage.getInstance();
 
         qr = root.findViewById(R.id.imgQR2);
-        button = root.findViewById(R.id.btnEscanearQR);
-        filePath = "android.resource://" + getActivity().getPackageName() + "/raw/tarea_semanal_02";
 
-
-        mostrarQR(filePath, qr);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                escanearQR();
-            }
-        });
-
+        subirPDF();
+        generarQR(qr);
         return root;
     }
 
@@ -92,82 +96,106 @@ public class HomeFragment extends Fragment {
 
         if (result != null && result.getContents() != null) {
             String qrContent = result.getContents();
-            Log.d("qr", qrContent);
+//            if (qrContent.equals()) {
+//                descargarPDFFromStorage();
+//            }
         }
     }
 
-    private void mostrarQR(String filePath, ImageView imageView) {
-        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-        try {
-            BitMatrix bitMatrix = multiFormatWriter.encode(filePath, BarcodeFormat.QR_CODE, 300, 300);
-            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-            imageView.setImageBitmap(bitmap);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private void subirPDF() {
+        InputStream inputStream = getResources().openRawResource(R.raw.tarea_semanal_02);
+        StorageReference pdfRef = mStorage.getReference().child("archivos/cartilla.pdf");
 
-    private void escanearQR() {
-        IntentIntegrator intentIntegrator = new IntentIntegrator(getActivity());
-        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-        intentIntegrator.setPrompt("Escanea el codigo QR");
-        intentIntegrator.setOrientationLocked(false);
-        intentIntegrator.setBeepEnabled(true);
-        intentIntegrator.initiateScan();
-    }
-
-    private void editarPdf(String filePath, List<Producto> listaProductos) {
-
-        try {
-            PdfReader reader = new PdfReader(filePath);
-            PdfStamper stamper = new PdfStamper(reader, new FileOutputStream("ruta/nuevo_pdf.pdf"));
-            AcroFields form = stamper.getAcroFields();
-
-            for (int i = 0; i < listaProductos.size(); i++) {
-                Producto producto = listaProductos.get(i);
-
-                form.setField("nombre_producto_" + i, producto.getNombre());
-                form.setField("descripcion_producto_" + i, producto.getDescripcion());
-
-                form.setGenerateAppearances(true);
-                stamper.setFormFlattening(true);
+        pdfRef.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+            @Override
+            public void onSuccess(StorageMetadata storageMetadata) {
+                Toast.makeText(getContext(), "El archivo ya existe", Toast.LENGTH_SHORT).show();
             }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                UploadTask uploadTask = pdfRef.putStream(inputStream);
+                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-            stamper.close();
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
     }
 
-    private void generarQRDescargarPDF() {
-        String filePath = "android.resource://" + getActivity().getPackageName() + "/raw/tarea_semanal_02";
-        String contenidoQR = filePath;
+    private void generarQR(ImageView imageView) {
+        StorageReference storageReference = mStorage.getReference().child(storagePathPdfCartilla);
 
-        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String storageUrl = uri.toString();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try {
 
-        try {
-            BitMatrix bitMatrix = multiFormatWriter.encode(contenidoQR, BarcodeFormat.QR_CODE, 300, 300);
-            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    BitMatrix bitMatrix = multiFormatWriter.encode(storageUrl, BarcodeFormat.QR_CODE, 300, 300);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
 
-            String qrImagePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
-            FileOutputStream outputStream = new FileOutputStream(qrImagePath);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-            outputStream.flush();
-            outputStream.close();
+                    imageView.setImageBitmap(bitmap);
+                    imageView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            descargarPDFFromStorage();
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Manejar el error de obtener la URL del archivo
+                e.printStackTrace();
+            }
+        });
 
-            File pdfFile = new File("ruta/nuevo_pdf.pdf");
-            Uri pdfUri = FileProvider.getUriForFile(getContext(), "com.paqta.paqtafood.fileprovider", pdfFile);
+    }
 
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(pdfUri, "application/pdf");
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivity(intent);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void descargarPDFFromStorage() {
+        StorageReference storageRef = mStorage.getReference().child(storagePathPdfCartilla);
+        File localFile = new File(getContext().getExternalFilesDir(null), "cartilla.pdf");
+
+        storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                // El archivo PDF se ha descargado exitosamente
+                Toast.makeText(getContext(), "PDF descargado correctamente", Toast.LENGTH_SHORT).show();
+
+                // Aquí puedes abrir el archivo PDF o realizar cualquier otra acción necesaria
+                // Por ejemplo, puedes abrirlo utilizando una aplicación de visor de PDF instalada en el dispositivo
+                openPDF(localFile);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Ocurrió un error al descargar el archivo PDF
+                Toast.makeText(getContext(), "Error al descargar el PDF", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void openPDF(File file) {
+        Uri fileUri = FileProvider.getUriForFile(getContext(), "com.paqta.paqtafood.fileprovider", file);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(fileUri, "application/pdf");
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
     }
 
 }
